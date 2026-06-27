74c92dbb0a1cf24156c4dca481f4255467cefba4
Zhendi Cao <metrixgo9@gmail.com>
2026-06-28 08:10:46 +0900

    Regular Commit 0.16.2
    Remade the tv thing

--- Assets/Scripts/NightScene/TVStart.cs
+++ Assets/Scripts/NightScene/TVStart.cs
@@ -18,13 +18,11 @@ public class TVStart : MonoBehaviour
     private AudioSource tv;
     private Renderer rend;
     private VideoPlayer vp;
-    private TVControl tvc;
     private bool getUp = false;
 
     private void Start()
     {
         tv = GetComponent<AudioSource>();
-        tvc = GetComponent<TVControl>();
         rend = GetComponent<Renderer>();
         vp = GetComponent<VideoPlayer>();
         MainManager.instance.AddTrigger("wait;" + (getOnSofa.length + 1.0f + turnOnTV.length + 1.5f));
@@ -34,13 +32,20 @@ public class TVStart : MonoBehaviour
 
     private void Update()
     {
-        if (getUp) return;
-
-        t += Time.deltaTime;
-        Vector3 pos = firstPlayer.transform.position;
-        pos.y = 1.35f + Mathf.Sin(t) / 100.0f;
-        pos.x = -50.85f + Mathf.Cos(1.7f * t) / 200.0f;
-        firstPlayer.transform.position = pos;
+        if (!getUp)
+        {
+            t += Time.deltaTime;
+            Vector3 pos = firstPlayer.transform.position;
+            pos.y = 1.35f + Mathf.Sin(t) / 100.0f;
+            pos.x = -50.85f + Mathf.Cos(1.7f * t) / 200.0f;
+            firstPlayer.transform.position = pos;
+        }
+        else if (MainManager.instance.HasItem("Control") && Input.GetKeyDown(KeyCode.E))
+        {
+            MainManager.instance.SetPrompt("");
+            Destroy(gameObject);
+        }
+        
     }
 
     private IEnumerator BeginShow()
@@ -117,7 +122,5 @@ public class TVStart : MonoBehaviour
         MainManager.instance.AddTrigger("canrun;1");
         Destroy(firstPlayer);
         player.SetActive(true);
-        tvc.enabled = true;
-        Destroy(this);
     }
 }

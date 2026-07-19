7ac9cb32b690747839c58edcea507ddf5d53566c
Zhendi Cao <metrixgo9@gmail.com>
2026-07-19 00:19:56 -0700

    Regular Commit 0.21.7
    Redoing all the sewer meshes because the stupid AI navigation can't let them jump on high stairs I hate you Unity

--- Assets/Scripts/SewerScene/SewerKiller.cs
+++ Assets/Scripts/SewerScene/SewerKiller.cs
@@ -3,40 +3,5 @@ using UnityEngine.AI;
 
 public class SewerKiller : MonoBehaviour
 {
-    [SerializeField] private Transform target;
-
-    private NavMeshAgent agent;
-    private Animator anim;
-    private float dis;
-    private float attackDis = 1.0f;
-    private float sightDis = 10.0f;
-    private float speed = 2.0f;
-    private float runSpeed = 4.5f;
-
-    private void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        anim = GetComponent<Animator>();
-    }
-
-    private void Update()
-    {
-        dis = Vector3.Distance(agent.transform.position, target.position);
-
-        if (dis < attackDis)
-        {
-            agent.isStopped = true;
-            anim.SetBool("Attack", true);
-        }
-        else
-        {
-            agent.isStopped = false;
-            anim.SetBool("Attack", false);
-            anim.SetBool("Run", dis < sightDis);
-            agent.destination = target.position;
-        }
-
-        if (anim.GetBool("Run")) agent.speed = runSpeed;
-        else agent.speed = speed;
-    }
+    
 }

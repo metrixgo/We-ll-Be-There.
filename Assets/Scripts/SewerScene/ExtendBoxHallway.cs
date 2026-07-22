24a9f2feaa0785b8f177509cd2c84e1ae5f7acec
Zhendi Cao <metrixgo9@gmail.com>
2026-07-22 12:28:53 -0700

    Regular Commit 0.22.2
    Added extend tubes
    
    Fixed some extending hallway issues
    
    Finished the translation

--- Assets/Scripts/SewerScene/ExtendBoxHallway.cs
+++ Assets/Scripts/SewerScene/ExtendBoxHallway.cs
@@ -19,16 +19,12 @@ public class ExtendBoxHallway : MonoBehaviour
     {
         if (!other.CompareTag("Player")) return;
 
-        MainManager.instance.PlayEffect(shift);
-        end.transform.Translate(Vector3.left * 12.0f, Space.World);
-        Instantiate(unit, pos, rot);
-        pos += Vector3.left * 12.0f;
         idx++;
-        if (idx >= 5)
+        if (idx >= 4)
         {
             playerHead.SetActive(true);
             playerHead.transform.parent = null;
-            playerHead.GetComponent<Rigidbody>().AddForce(-1.0f, 1.0f, 0, ForceMode.Impulse);
+            playerHead.GetComponent<Rigidbody>().AddForce(0, 0.5f, 0, ForceMode.Impulse);
             playerHead.GetComponent<Rigidbody>().AddTorque(Vector3.up / 3.0f, ForceMode.Impulse);
             player.SetActive(false);
             MainManager.instance.PlayEffect(die);
@@ -38,6 +34,10 @@ public class ExtendBoxHallway : MonoBehaviour
         }
         else
         {
+            MainManager.instance.PlayEffect(shift);
+            end.transform.Translate(Vector3.left * 12.0f, Space.World);
+            Instantiate(unit, pos, rot);
+            pos += Vector3.left * 12.0f;
             MainManager.instance.AddTrigger("flashscreen;#FF000000;#FF000055;0.2");
             MainManager.instance.AddTrigger("flashscreen;#FF000055;#FF000000;1");
         }

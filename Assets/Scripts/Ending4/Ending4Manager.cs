f92ee696d86986115e53c0b7e41bdc0cccc00ce6
Zhendi Cao <metrixgo9@gmail.com>
2026-08-15 23:20:00 -0700

    Regular Commit 0.25.13
    Changed all endings template

--- Assets/Scripts/Ending4/Ending4Manager.cs
+++ Assets/Scripts/Ending4/Ending4Manager.cs
@@ -85,7 +85,8 @@ public class Ending4Manager : MonoBehaviour
         yield return new WaitForSeconds(2.0f);
         policeAnim.SetBool("turn", false);
         yield return new WaitUntil(() => MainManager.instance.gameState == 1);
-        MainManager.instance.AddTrigger("wait;" + (3 + jumpscare.length));
+        MainManager.instance.AddTrigger("wait;2");
+        MainManager.instance.AddTrigger("changescreen;#FF000066;#FF000000;" + (1 + jumpscare.length));
         MainManager.instance.AddTrigger("dialogue;Mayor;It is about time.");
         yield return new WaitForSeconds(2.0f);
         cam.position = new Vector3(-74.2829971f, 2.2f, -66.23f);
@@ -93,6 +94,7 @@ public class Ending4Manager : MonoBehaviour
         killer.SetActive(true);
         MainManager.instance.PlayMusic(tense);
         MainManager.instance.PlayEffect(jumpscare);
+
         yield return new WaitUntil(() => MainManager.instance.gameState == 1);
         MainManager.instance.AddTrigger("wait;5");
         MainManager.instance.AddTrigger("changescreen;#000000FF;#00000000;5");
@@ -135,6 +137,7 @@ public class Ending4Manager : MonoBehaviour
         }
         MainManager.instance.PlayEffect(lightsOut);
         MainManager.instance.StopMusic();
+        subTense.Stop();
         RenderSettings.ambientIntensity = 0;
         RenderSettings.fogDensity = 1.0f;
         yield return new WaitForSeconds(2.0f);
@@ -147,15 +150,18 @@ public class Ending4Manager : MonoBehaviour
         pc.gameObject.SetActive(false);
         jumpscareCam.SetActive(true);
         RenderSettings.fogDensity = 0.7f;
-        RenderSettings.ambientIntensity = 1.0f;
-        subTense.clip = tinnitus;
-        subTense.Play();
+        RenderSettings.ambientIntensity = 0.3f;
+        MainManager.instance.PlayEffect(jumpscare);
         MainManager.instance.StopMusic();
-        MainManager.instance.AddTrigger("wait;0.1");
+        MainManager.instance.AddTrigger("wait;0.3");
         MainManager.instance.AddTrigger("changescreen;#FF0000FF;#FF0000FF;3");
         MainManager.instance.AddTrigger("changescreen;#FF0000FF;#000000FF;3");
         MainManager.instance.AddTrigger("wait;5");
-        yield return new WaitForSeconds(3.1f);
+        yield return new WaitForSeconds(0.3f);
+        subTense.clip = tinnitus;
+        subTense.Play();
+        MainManager.instance.StopEffect();
+        yield return new WaitForSeconds(3.0f);
         t = 0;
         while(t < 3.0f)
         {
@@ -177,7 +183,7 @@ public class Ending4Manager : MonoBehaviour
             yield return null;
         }
         title.color = Color.clear;
-        MainManager.instance.DisplayEnding("FINAL ENDING 4/5 - We'll Be There.", "You managed to survive to the end. You went through all the hallucinations. You realized they would be here. You faced your ending calmly. You knew it was going to happen. You've paid back your mistake.");
+        MainManager.instance.DisplayEnding("FINAL ENDING 4/5 - We'll Be There.", "You managed to survive to the end. You went through all the hallucinations. You realized they would be here. You faced your ending calmly. You knew it was going to happen. You've paid back your mistake.", 1);
     }
 
     private IEnumerator DisplayMove(string speaker, string content)
